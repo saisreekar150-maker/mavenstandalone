4b21f4e3cee724f02fb60b3f14743770b06cf062
githublearning <sai.sreekar150@gmail.com>
2026-01-06 12:49:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 this is new file
-previously committed files are moved to staging are
+previously committed files are moved to staging area
+remote to local repository firstly use git remote -v
+git pull surl master

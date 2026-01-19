ad2896ffe25cb7a67a415181f997b7b50bb5ed2c
sai.sreekar150 <sai.sreekar150@gmail.com>
2026-01-19 12:31:09 +0000

    done

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-this is new file
-previously committed files are moved to staging area
-remote to local repository firstly use git remote -v
-git pull surl master
+added new line over dev branch
+file saved
